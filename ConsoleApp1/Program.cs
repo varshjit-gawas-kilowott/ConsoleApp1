0134db78182cc9c8d7c878396985ebbb9ca18c02
varshjit <89901335+varshjit-gawas-kilowott@users.noreply.github.com>
2021-11-29 17:02:27 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Bob";
+            int messages = 3;
+            decimal temperature = 34.4m;
+
+            Console.Write("Hello, ");
+            Console.Write(name);
+            Console.Write("! You have ");
+            Console.Write(messages);
+            Console.Write(" in your inbox. The temperature is ");
+            Console.Write(temperature);
+            Console.Write(" celsius.");
+        }
+    }
+}
