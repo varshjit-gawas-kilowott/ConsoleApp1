457f3123113e3b3c2afdac2b9624ca7b46a6cf38
varshjit <89901335+varshjit-gawas-kilowott@users.noreply.github.com>
2021-11-29 17:35:58 +0530

    fifth challenge

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,11 +10,33 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int firstValue = 500;
-            int secondValue = 600;
-            int largerValue;
-            largerValue = Math.Max(firstValue, secondValue);
-            Console.WriteLine(largerValue);
+            Random random = new Random();
+            int daysUntilExpiration = random.Next(12);
+            int discountPercentage = 0;
+
+            if (daysUntilExpiration == 0)
+            {
+                Console.WriteLine("Your subscription has expired.");
+            }
+            else if (daysUntilExpiration == 1)
+            {
+                Console.WriteLine("Your subscription expires within a day!");
+                discountPercentage = 20;
+            }
+            else if (daysUntilExpiration <= 5)
+            {
+                Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+                discountPercentage = 10;
+            }
+            else if (daysUntilExpiration <= 10)
+            {
+                Console.WriteLine("Your subscription will expire soon.  Renew now!");
+            }
+
+            if (discountPercentage > 0)
+            {
+                Console.WriteLine($"Renew now and save {discountPercentage}%.");
+            }
         }
     }
 }
