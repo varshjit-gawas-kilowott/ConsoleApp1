dabb1185335d28b8dbee889fdf3e15cbea0adb97
varshjit <89901335+varshjit-gawas-kilowott@users.noreply.github.com>
2021-11-29 17:28:03 +0530

    Third challenge

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,13 +10,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string projectName = "ACME";
-            string englishLocation = $@"c:\Exercise\{projectName}\data.txt";
-            Console.WriteLine($"View English output:\n\t\t{englishLocation}\n");
-
-            string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
-            string russianLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
-            Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
+            int fahrenheit = 94;
+            decimal celsius = (fahrenheit - 32m) * (5m / 9m);
+            Console.WriteLine("The temperature is " + celsius + " Celsius.");
         }
     }
 }
