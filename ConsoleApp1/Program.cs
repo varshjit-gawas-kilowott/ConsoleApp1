b678790920e095e7827e52670f4dc308cebc12e8
varshjit <89901335+varshjit-gawas-kilowott@users.noreply.github.com>
2021-11-29 17:19:01 +0530

    Second challenge

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,17 +10,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string name = "Bob";
-            int messages = 3;
-            decimal temperature = 34.4m;
+            string projectName = "ACME";
+            string englishLocation = $@"c:\Exercise\{projectName}\data.txt";
+            Console.WriteLine($"View English output:\n\t\t{englishLocation}\n");
 
-            Console.Write("Hello, ");
-            Console.Write(name);
-            Console.Write("! You have ");
-            Console.Write(messages);
-            Console.Write(" in your inbox. The temperature is ");
-            Console.Write(temperature);
-            Console.Write(" celsius.");
+            string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+            string russianLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
+            Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
         }
     }
 }
